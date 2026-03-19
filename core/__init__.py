b531d593f93b9a7f3a7561e04acdec8b2d8764ba
Sakura <150332048+Sakuraguayu@users.noreply.github.com>
2026-03-19 15:10:39 +0800

    Add files via upload

---
+++ core/__init__.py
@@ -0,0 +1,11 @@
+from .bilibili import BilibiliParser
+from .git import GitParser
+from .douyin import DouyinParser
+from .screenshot import ScreenshotParser
+from .acfun import AcFunParser
+from .weibo import WeiboParser
+from .nga import NGAParser
+from .xhs import XHSparser
+from .youtube import YoutubeParser
+
+__all__ = ["BilibiliParser", "GitParser", "DouyinParser", "ScreenshotParser", "AcFunParser", "WeiboParser", "NGAParser", "XHSparser", "YoutubeParser"]
